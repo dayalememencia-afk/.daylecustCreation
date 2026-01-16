c7d59ebc4d02cbac069910a07bbec5e9b93e2d52
dayalememencia-afk <dayalememencia@gmail.com>
2026-01-16 15:12:30 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,9 +3,11 @@ module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  if (req.method === 'OPTIONS') return res.status(200).end();
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
 
-  res.status(200).json({
+  const response = {
     jsonrpc: "2.0",
     id: req.body?.id || 1,
     result: {
@@ -13,9 +15,14 @@ module.exports = async (req, res) => {
         {
           name: "saudacao_mestra",
           description: "Saudação oficial para a Mestra Day",
-          inputSchema: { type: "object", properties: {} }
+          inputSchema: {
+            type: "object",
+            properties: {}
+          }
         }
       ]
     }
-  });
+  };
+
+  return res.status(200).json(response);
 };
